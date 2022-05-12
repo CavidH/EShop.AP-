b1f47a4be43ff4351cee7f1957ccdc0a8cfee71f
Cavid Haciyev <cavid.haciyev.2002@mail.ru>
2022-05-12 17:23:46 +0400

    dbset

--- İnfrastructure/EShopAPİ.Persistence/Contexts/EShopDbContext.cs
+++ İnfrastructure/EShopAPİ.Persistence/Contexts/EShopDbContext.cs
@@ -1,9 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EShopAPİ.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace EShopAPİ.Persistence.Contexts
 {
     public class EShopDbContext : DbContext
     {
-        public EShopDbContext(DbContextOptions options) : base(options)  { }
+        public EShopDbContext(DbContextOptions options) : base(options) { }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+
+
+
     }
 }

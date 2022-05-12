3cfe5f62a7d076bf72b6e8b722921149712b3018
Cavid Haciyev <cavid.haciyev.2002@mail.ru>
2022-05-12 17:22:07 +0400

    Db context

---
+++ İnfrastructure/EShopAPİ.Persistence/Contexts/EShopDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EShopAPİ.Persistence.Contexts
+{
+    public class EShopDbContext : DbContext
+    {
+        public EShopDbContext(DbContextOptions options) : base(options)  { }
+    }
+}
